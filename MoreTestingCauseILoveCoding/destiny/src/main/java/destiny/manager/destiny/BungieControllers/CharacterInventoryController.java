f88480e3cdaad7e92e3087d7e914a07255f32e1d
Adam Dickinson <adickinson@retrorabbit.co.za>
2023-04-09 16:22:15 +0200

    html work and viewing of data

--- MoreTestingCauseILoveCoding/destiny/src/main/java/destiny/manager/destiny/BungieControllers/CharacterInventoryController.java
+++ MoreTestingCauseILoveCoding/destiny/src/main/java/destiny/manager/destiny/BungieControllers/CharacterInventoryController.java
@@ -5,17 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 import destiny.manager.destiny.BungieServices.GetCharacterInventory;
 
@@ -36,12 +30,14 @@ public class CharacterInventoryController {
             Integer light = characterJson.getJSONObject("Response").getJSONObject("character").getJSONObject("data").getInt("light");
             Integer raceType = characterJson.getJSONObject("Response").getJSONObject("character").getJSONObject("data").getInt("raceType");
             Integer classType = characterJson.getJSONObject("Response").getJSONObject("character").getJSONObject("data").getInt("classType");
+            String emblemBackgroundPath = characterJson.getJSONObject("Response").getJSONObject("character").getJSONObject("data").getString("emblemBackgroundPath");
 
             Map<String, Object> characterData = new HashMap<>();
             characterData.put("characterId", characterId);
             characterData.put("light", light);
             characterData.put("raceType", raceType);
             characterData.put("classType", classType);
+            characterData.put("emblemBackgroundPath", emblemBackgroundPath);
             characterInfo.add(characterData);
         }
 
